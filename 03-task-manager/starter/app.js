cec5d027f9e719b14e5a81564387074ebd56bb09
Iryna <iryna.kolhanova@gmail.com>
2023-11-05 10:15:17 -0500

    answers for lesson 6 - Task Manager API Part 2

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,18 +3,31 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDb = require("./db/connect");
 require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+require("express-async-errors");
 
-const port = 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+
+app.use(errorHandler);
+
+// Additional Assignment
+// app.use((err, req, res, next) => {
+//   if (err.message === "Not found") {
+//     res.status(404);
+//     res.json({ error: err.message });
+//   } else {
+//     res.status(500);
+//     res.json({ error: "Something went wrong" });
+//   }
+//   next(err);
+// });
 
 const start = async () => {
   try {
