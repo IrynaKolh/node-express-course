94d2fe7b0a18b64cbfc1ebc5eed2136635e1eb53
Iryna <iryna.kolhanova@gmail.com>
2023-11-12 11:52:12 -0500

    answers for lesson 7 - Using Query Parameters

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+const express = require("express");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+require("express-async-errors");
+require("dotenv").config();
+const productsRouter = require("./routes/products");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
